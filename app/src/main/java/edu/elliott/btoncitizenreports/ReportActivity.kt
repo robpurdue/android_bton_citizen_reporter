b56346740449e4cfbd7c2154e581b844589a34f8
Rob Elliott <elliott@iupui.edu>
2020-07-30 11:13:16 -0400

    Clicking on Map allows writing to Firebase

--- app/src/main/java/edu/elliott/btoncitizenreports/ReportActivity.kt
+++ app/src/main/java/edu/elliott/btoncitizenreports/ReportActivity.kt
@@ -5,7 +5,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 import edu.elliott.btoncitizenreports.databinding.ActivityReportBinding
+import kotlin.random.Random
 
 private const val REPORT_LATITUDE = "edu.elliott.btoncitizenreports.report_latitude"
 private const val REPORT_LONGITUDE = "edu.elliott.btoncitizenreports.report_longitude"
@@ -17,6 +19,7 @@ class ReportActivity : AppCompatActivity() {
     private var longitude: String? = ""
     private var address: String? = ""
     private lateinit var binding: ActivityReportBinding
+    lateinit var firestore: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,9 +35,22 @@ class ReportActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.reportTextView.text = makeReportDescription()
 
+        // SET CLICK LISTENERS FOR BUTTONS
+        binding.injury.setOnClickListener { writeToFirestore("injury") }
+        binding.line.setOnClickListener { writeToFirestore("line") }
+        binding.outage.setOnClickListener { writeToFirestore("outage") }
+        binding.fire.setOnClickListener { writeToFirestore("fire") }
+        binding.blocked.setOnClickListener { writeToFirestore("blocked") }
+
         Log.d(TAG, latitude!!)
         Log.d(TAG, longitude!!)
         Log.d(TAG, address!!)
+
+        // Enable Firestore logging
+        FirebaseFirestore.setLoggingEnabled(true)
+
+        // Firestore
+        firestore = initFirestore()
 
     }
 
@@ -45,6 +61,40 @@ class ReportActivity : AppCompatActivity() {
         desc = desc + "Approximate Address: " + address + "\n\n"
         desc = desc + "If this is not correct, go back and tap a different location."
         return desc
+    }
+
+    fun initFirestore(): FirebaseFirestore {
+        return FirebaseFirestore.getInstance()
+    }
+
+    fun writeToFirestore(reportType : String) {
+        Log.d(TAG, "In writeToFirestore()")
+        // Figure out what type of report we are making
+
+        val description = when (reportType) {
+            "injury" -> "Personal Injury"
+            "line" -> "Downed Power Line"
+            "outage" -> "Power Outage"
+            "fire" -> "Fire"
+            "blocked" -> "Blocked Roadway"
+            else -> "Unknown"
+        }
+
+        val docData = hashMapOf(
+            "description" to description,
+            "icon" to reportType,
+            "latitude" to latitude,
+            "longitude" to longitude
+        )
+        firestore.collection("markers")
+            .add(docData)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot written with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
     }
 
     companion object {
